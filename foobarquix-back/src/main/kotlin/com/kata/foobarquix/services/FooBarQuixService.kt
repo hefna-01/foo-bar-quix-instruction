94e4b70a1791eee54bf0dcbc523a96e68cd3f8e0
hefna-01 <handrinirina@gmail.com>
2020-12-03 16:10:15 +0100

    commit service and TU service

--- foobarquix-back/src/main/kotlin/com/kata/foobarquix/services/FooBarQuixService.kt
+++ foobarquix-back/src/main/kotlin/com/kata/foobarquix/services/FooBarQuixService.kt
@@ -6,7 +6,41 @@ import org.springframework.stereotype.Component
 class FooBarQuixService {
 
     fun convertNumber(inputNumber: Int): String {
-        return ""
+        var retValue = ""
+        var stringValue = inputNumber.toString()
+        var divideByThree = isDivideByThree(inputNumber);
+        var divideByFive = isDivideByFive(inputNumber);
+        if(divideByThree) {
+            retValue = "Foo"
+        }
+        if(divideByFive) {
+            retValue = retValue.plus("Bar")
+        }
+        val chars: Array<Char> = stringValue.toCharArray().toTypedArray()
+        for(char: Char in chars) {
+            if(char.toString() == "3"){
+                retValue = retValue.plus("Foo")
+            }
+            if(char.toString() == "5"){
+                retValue = retValue.plus("Bar")
+            }
+            if(char.toString() == "7"){
+                retValue = retValue.plus("Quix")
+            }
+        }
+        if (!stringValue.contains("3") && !stringValue.contains("5") && !stringValue.contains("7") && !divideByThree
+                && !divideByFive){
+            retValue =  retValue.plus(stringValue)
+        }
+        return retValue
+    }
+
+    fun isDivideByThree(number: Int): Boolean {
+        return number % 3 == 0;
+    }
+
+    fun isDivideByFive(number: Int): Boolean {
+        return number % 5 == 0;
     }
 
 }
--- foobarquix-back/src/test/kotlin/com/kata/foobarquix/FooBarQuixApplicationTests.kt
+++ foobarquix-back/src/test/kotlin/com/kata/foobarquix/FooBarQuixApplicationTests.kt
@@ -1,13 +1,51 @@
 package com.kata.foobarquix
 
+import com.kata.foobarquix.services.FooBarQuixService
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class FooBarQuixApplicationTests {
 
+	@Autowired
+	private val fooBarQuixService: FooBarQuixService? = null
+
 	@Test
 	fun contextLoads() {
+		assertThat(fooBarQuixService).isNotNull;
 	}
 
+	@Test
+	fun isDivideByThreeTest() {
+		if (fooBarQuixService != null) {
+			assertThat(fooBarQuixService.isDivideByThree(1)).isEqualTo(false);
+			assertThat(fooBarQuixService.isDivideByThree(3)).isEqualTo(true);
+		}
+	}
+
+	@Test
+	fun isDivideByFiveTest() {
+		if (fooBarQuixService != null) {
+			assertThat(fooBarQuixService.isDivideByFive(1)).isEqualTo(false);
+			assertThat(fooBarQuixService.isDivideByFive(5)).isEqualTo(true);
+		}
+	}
+
+	@Test
+	fun convertNumberTest() {
+		if (fooBarQuixService != null) {
+			assertThat(fooBarQuixService.convertNumber(1)).isEqualTo("1");
+			assertThat(fooBarQuixService.convertNumber(3)).isEqualTo("FooFoo");
+			assertThat(fooBarQuixService.convertNumber(5)).isEqualTo("BarBar");
+			assertThat(fooBarQuixService.convertNumber(7)).isEqualTo("Quix");
+			assertThat(fooBarQuixService.convertNumber(9)).isEqualTo("Foo");
+			assertThat(fooBarQuixService.convertNumber(51)).isEqualTo("FooBar");
+			assertThat(fooBarQuixService.convertNumber(53)).isEqualTo("BarFoo");
+			assertThat(fooBarQuixService.convertNumber(33)).isEqualTo("FooFooFoo");
+			assertThat(fooBarQuixService.convertNumber(27)).isEqualTo("FooQuix");
+			assertThat(fooBarQuixService.convertNumber(15)).isEqualTo("FooBarBar");
+		}
+	}
 }
